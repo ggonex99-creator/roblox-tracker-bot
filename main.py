a7d5c64907133d21254624f560fe1b6ec9843b36
ggonex99-creator <ggonex99@gmail.com>
2026-07-24 05:15:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import requests
+import time
+from datetime import datetime
 
+USERNAME = "kiti1honey"
+
+while True:
+    print(f"Checking {USERNAME}...")
+    print("Bot is running successfully!")
+    time.sleep(30)
